cd53d4aa9a2ccb40150705255d5193f825d64654
Md Najim Islam <najimoffice21@gmail.com>
2026-04-14 14:38:46 +0600

    Add Menu with Loop or Switch and Add Student spread operator

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -5,9 +5,47 @@ const String appTitle = "Student Grader v1.0";
 final Set<String> availableSubjects = {"Math", "English", "Science", "ICT"};
 
 // Add Student
+void main() {
+  var students = <Map<String, dynamic>>[];
+  var running = true;
+
+  do {
+    print("""
+===== $appTitle =====
+
+1. Add Student
+
+
+Choose an option:
+""");
+
+var choice = stdin.readLineSync();
+
+    switch (choice) {
+      case "1":
+        addStudent(students);
+        break;
+
+      case "2":
+        running = false;
+        print("Exiting...");
+        break;
+
+      default:
+        print("Invalid option");
+    }
+  } while (running);
+}
+
+//3. Add Student Function
 void addStudent(List<Map<String, dynamic>> students) {
-  stdout.write("Enter name: ");
-  var name = stdin.readLineSync();
+  stdout.write("Enter student name: ");
+  String? name = stdin.readLineSync();
+
+  if (name == null || name.isEmpty) {
+    print("Name cannot be empty!");
+    return;
+  }
 
   var student = {
     "name": name,
@@ -18,16 +56,8 @@ void addStudent(List<Map<String, dynamic>> students) {
   };
 
   students.add(student);
-  print("Student $name added!");
+
+  print("Student '$name' added successfully!");
+ 
 }
 
-void main() {
-  print(appTitle);
-  print(availableSubjects);
-
-  var students = <Map<String, dynamic>>[];
-
-  addStudent(students); // call function
-
-  print(students);
-}
