d22bce0530a8a665a73b8bbc7034d439cb229dc4
Md Najim Islam <najimoffice21@gmail.com>
2026-04-14 11:45:21 +0600

    Add project files

---
+++ bin/cli_student_grader.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  // App Setup
+  const String appTitle = "Student Grader v1.0";
+  final Set<String> availableSubject = {"Math", "English", "Science", "ICT"};
+  print(appTitle);
+  print(availableSubject);
+
+  var students = <Map<String, dynamic>>[];
+  var running = true;
+
+  print(students);
+  print(running);
+
+
+
+}
