2c3ed1f3f382182389e9703d75bfdd2d8f078a99
Md Najim Islam <najimoffice21@gmail.com>
2026-04-14 13:47:37 +0600

    Add Student

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -1,18 +1,33 @@
 import 'dart:io';
 
+// App Setup
+const String appTitle = "Student Grader v1.0";
+final Set<String> availableSubjects = {"Math", "English", "Science", "ICT"};
+
+// Add Student
+void addStudent(List<Map<String, dynamic>> students) {
+  stdout.write("Enter name: ");
+  var name = stdin.readLineSync();
+
+  var student = {
+    "name": name,
+    "scores": <int>[],
+    "subjects": {...availableSubjects},
+    "bonus": null,
+    "comment": null,
+  };
+
+  students.add(student);
+  print("Student $name added!");
+}
+
 void main() {
-  // App Setup
-  const String appTitle = "Student Grader v1.0";
-  final Set<String> availableSubject = {"Math", "English", "Science", "ICT"};
   print(appTitle);
-  print(availableSubject);
+  print(availableSubjects);
 
   var students = <Map<String, dynamic>>[];
-  var running = true;
+
+  addStudent(students); // call function
 
   print(students);
-  print(running);
-
-
-
 }
